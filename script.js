0a9f733dee500b8633e51060eb6cae25382dc684
Irshad-V <irshadvtl272@gmail.com>
2023-03-12 21:36:18 +0530

    Ui completed

---
+++ script.js
@@ -0,0 +1,8 @@
+const form = document.getElementById("shop-form")
+const itemName = document.getElementById("item-name")
+const itemqty = document.getElementById("item-qty")
+const list = document.getElementById("list")
+form.addEventListener("submit", function (event) {
+    event.preventDefault()
+
+})
