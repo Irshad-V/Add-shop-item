03dba35e486734105d324d1163e1b6b36fa20f5f
Irshad-V <irshadvtl272@gmail.com>
2023-03-12 23:11:10 +0530

    item adding task completed

--- script.js
+++ script.js
@@ -2,7 +2,36 @@ const form = document.getElementById("shop-form")
 const itemName = document.getElementById("item-name")
 const itemqty = document.getElementById("item-qty")
 const list = document.getElementById("list")
+const alertBox = document.getElementById("alertBox")
 form.addEventListener("submit", function (event) {
     event.preventDefault()
+    if (itemName.value !== "" && itemqty.value !== "") {
+        const tr = document.createElement("tr")
+        const rand = Math.floor((Math.random() * 1000) + 1)
+        tr.setAttribute("id",`${rand}`)
+        tr.innerHTML = ` 
+        <td>${rand}</td>
+        <td>${itemName.value}</td>
+        <td>${itemqty.value}</td>
+        
+        <td> <button class="btn btn-danger">Delete</button> </td>
+        `
+        list.appendChild(tr)
+        itemName.value =""
+        itemqty.value =" "
+        alertBox.classList.add('alert-success')
+        alertBox.innerHTML ="Item Added Successfully !";
+        setTimeout(()=>{
+            alertBox.classList.remove("alert-success")
+            alertBox.innerHTML =""
+        },3000)
+    }else{
+        alertBox.classList.add("alert-danger")
+        alertBox.innerHTML="Please Add Items !"
+        setTimeout(()=>{
+            alertBox.classList.remove("alert-danger")
+            alertBox.innerHTML =""
+        },3000)
+    }
 
 })
